6288bde1423aaf13455356c6389897c75040307a
Coy McNew <mcnewcp@gmail.com>
2020-12-31 07:54:46 -0600

    changed source to tigris package

--- generate_census_tracts.R
+++ generate_census_tracts.R
@@ -1,13 +1,19 @@
-### this script reads in the TN census tract shapefile from census.gov and generates a
-### simplified rds of Davidson County for use in R elsewhere
-### raw data is downloaded from https://www.census.gov/cgi-bin/geo/shapefiles/
+### this script uses tigris package to save an RDS of census polygon data for quick loading
+### from github
 if(!require(pacman)) install.packages('pacman')
-p_load(tidyverse, sf)
+p_load(tigris, tidyverse, sf)
 
-mapSF <- st_read(file.path("tl_2019_47_tract", "tl_2019_47_tract.shp")) %>%
+#load census tracts for Davidson County, TN
+t <- tracts("TN", "Davidson", year = 2019) %>%
   rename_all(tolower) %>%
-  filter(countyfp == "037") %>% #filter to Davidson County
-  st_transform(4326) %>% #transform all to lat/lon
-  select(geoid, census_tract = name) 
+  st_transform(4326) %>% #transform to lat/lon
+  select(geoid, census_tract = name)
 
-write_rds(mapSF, "Nashville_Census_Tracts_2019.RDS")
+#write to RDS
+write_rds(t, "Nashville_Census_Tracts_2019.RDS")
+
+# #### TEMP CHECK ######
+# library(leaflet)
+# leaflet(t) %>%
+#   addTiles() %>%
+#   addPolygons()
