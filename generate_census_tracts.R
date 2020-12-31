1cee8523098d83ba79bf1d3be47ed09d71e5a881
Coy McNew <mcnewcp@gmail.com>
2020-12-31 08:41:37 -0600

    added block groups and voting districts

--- generate_census_tracts.R
+++ generate_census_tracts.R
@@ -4,16 +4,41 @@ if(!require(pacman)) install.packages('pacman')
 p_load(tigris, tidyverse, sf)
 
 #load census tracts for Davidson County, TN
-t <- tracts("TN", "Davidson", year = 2019) %>%
+t <- tracts("TN", "Davidson", year = 2019, cb = TRUE) %>%
   rename_all(tolower) %>%
   st_transform(4326) %>% #transform to lat/lon
-  select(geoid, census_tract = name)
+  mutate(name = paste("Census Tract:", tractce)) %>%
+  select(geoid, name)
+
+#load census block groups for Davidson County, TN
+bg <- block_groups("TN", "Davidson", year = 2019, cb = TRUE) %>%
+  rename_all(tolower) %>%
+  st_transform(4326) %>% #transform to lat/lon
+  mutate(name = paste("Census Tract:", tractce, "<br/>Census Block Group:", blkgrpce)) %>%
+  select(geoid, name)
+
+# #load census blocks for Davidson County, TN
+# #this sf is >100 MB
+# b <- blocks("TN", "Davidson", year = 2019) %>%
+#   rename_all(tolower) %>%
+#   st_transform(4326) #transform to lat/lon
+
+#load census block groups for Davidson County, TN
+vd <- voting_districts("TN") %>%
+  rename_all(tolower) %>%
+  filter(countyfp10 == "037") %>%
+  st_transform(4326) %>% #transform to lat/lon
+  mutate(name = paste("Voting District:", name10)) %>%
+  select(geoid10, name)
 
 #write to RDS
-write_rds(t, "Nashville_Census_Tracts_2019.RDS")
+write_rds(
+  list(tract = t, block_group = bg, voting_district = vd), 
+  "Nashville_Census_Polygons_2019.RDS"
+)
 
 # #### TEMP CHECK ######
 # library(leaflet)
-# leaflet(t) %>%
+# leaflet(vd) %>%
 #   addTiles() %>%
 #   addPolygons()
